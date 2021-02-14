c90a234452f80d7e8f042201ee88107fab28c20a
Vladimir <dementev438@gmail.com>
2021-02-14 23:24:48 +0700

    add class

---
+++ Snake/Const.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Const
+    {
+        public enum Point { SPACE, BORDER, SNAKE, HEAD, APPLE };
+    }
+}
---
+++ Snake/Field.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static Snake.Const;
+
+namespace Snake
+{
+    class Field
+    {
+        Point[][] arr;
+
+        public Field()
+        {
+            arr = new Point[100][];
+        }
+    }
+}
